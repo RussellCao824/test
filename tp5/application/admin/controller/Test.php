19aa616754a553d63693f8b52f1a9c8659f0d388
RussellCao824 <stczn824@163.com>
2018-10-30 10:02:48 +0800

    add the code of tp5

---
+++ tp5/application/admin/controller/Test.php
@@ -0,0 +1,12 @@
+<?php
+    namespace app\admin\controller;
+    use think\View;
+    class Test
+    {
+        public function demo1()
+        {
+//            $view = new View();
+//            return $view->fetch('demo');
+            return "it is the use of cross-namespace";
+        }
+    }
